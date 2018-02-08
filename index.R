c36221879d554ead7d2f416c8d251d7dc41a1dbb
pan <bpan@northern.co>
2018-02-08 17:02:26 -0500

    added new version

--- index.R
+++ index.R
@@ -1,23 +1,62 @@
+library(tidyverse)
 library(caret)
-library(ranger)
+library(randomForest)
+library(xgboost)
+library(mice)
+library(h2o)
+library(mxnet)
 
-train_data <- read.csv('pml-training.csv')
+training<-read_csv("pml-training.CSV")
+testing<-read_csv("pml-testing.CSV")
 
-summary(train_data)
-head(train_data$accel_dumbbell_x)
+set.seed(0)
 
-inTrain = createDataPartition(train_data$classe, p = 3/4)[[1]]
-training = train_data[inTrain,]
-testing = train_data[-inTrain,]
+str(testing)
+training<-training[,!sapply(testing,anyNA)]
+testing<-testing[,!sapply(testing,anyNA)]
+str(testing)
 
-colnames(training)
+factors<-c("X1","user_name","raw_timestamp_part_1","raw_timestamp_part_2","new_window","num_window")
 
-mod_rf <- train(classe~accel_forearm_x+accel_forearm_y+accel_forearm_z+
-                  accel_arm_x+accel_arm_y+accel_arm_z+
-                  accel_belt_x+accel_belt_y+accel_belt_z+
-                  accel_dumbbell_x+accel_dumbbell_y+accel_dumbbell_z
-  , data=training, method="ranger")
+training<-training[,!names(training) %in% factors]
+testing<-testing[,!names(testing) %in% c(factors,"problem_id")]
 
-pred_rf <- predict(mod_rf,testing)
+sum(is.na(training))
 
-confusionMatrix(pred_rf,testing$classe)
+mod<-mice(training)
+training<-complete(mod)
+
+inTrain<-createDataPartition(training$classe,p=.7,list=F)
+
+train<-training[inTrain,]
+validation<-training[-inTrain,]
+
+rm(inTrain,training,factors,mod)
+
+
+#random Forest  
+rfmodel<-train(classe~.,model='rf',train)
+rfresult<-predict(rfmodel,validation)
+confusionMatrix(rfresult$,validation$classe)
+
+#h2o
+h2o.train<-as.h2o(train)
+h2o.validation<-as.h2o(validation)
+
+h2o.model<-h2o.deeplearning(x=setdiff(colnames(train),"classe"),
+                            y="classe",
+                            training_frame = h2o.train,
+                            standardize = T,
+                            hidden = c(100,100),
+                            rate = 0.05,
+                            epochs = 100,
+                            seed=0
+)
+
+h2oresult<-h2o.predict(h2o.model,h2o.validation)
+confusionMatrix(h2oresult$prediction,validation$classe)
+
+#xgboost
+
+
+#xmnet
