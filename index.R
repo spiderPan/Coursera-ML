0f597cb94b5a83b4ba19e74b3998c671bfcbb14f
Owen Ouyang <oouyang@J2PMR3212.wattsworth.com>
2018-02-10 01:57:10 -0500

    xgboost

--- index.R
+++ index.R
@@ -3,11 +3,13 @@ library(ranger)
 library(tidyverse)
 library(GGally)
 library(corrplot)
+library(xgboost)
+library(vtreat)
+library(magrittr)
 
-train_data <- read.csv('pml-training.csv')
+train_data <- read_csv('pml-training.csv')
 
 summary(train_data)
-head(train_data$accel_dumbbell_x)
 
 train_cols <- c("accel_forearm_x",
                 "accel_forearm_y",
@@ -22,27 +24,43 @@ train_cols <- c("accel_forearm_x",
                 "accel_dumbbell_y",
                 "accel_dumbbell_z",
                 "classe")
+
 train_data<- train_data[,colnames(train_data) %in% train_cols]
 head(train_data)
 
-ggpairs(train_data[,c(1:7,13)],aes(col=classe,alpha=.4))
+#ggpairs(train_data[,c(1:7,13)],aes(col=classe,alpha=.4))
 
-corrplot(train_data,method='square')
+#corrplot(train_data,method='square')
 
 inTrain = createDataPartition(train_data$classe, p = 3/4)[[1]]
 training = train_data[inTrain,]
 testing = train_data[-inTrain,]
 
+treatplan<-designTreatmentsZ(training,train_cols[-13],verbose = F)
+new_vars<-treatplan%>%
+  use_series(scoreFrame)%>%
+  filter(code %in% c("lev","clean"))%>%
+  use_series(varName)
 
+training.treat<-prepare(treatplan,training,varRestriction = new_vars)
+testing.treat<-prepare(treatplan,testing,varRestriction = new_vars)
 
-colnames(training)
+training$classe<-training$classe %>% as.factor()
 
-mod_rf <- train(classe~accel_forearm_x+accel_forearm_y+accel_forearm_z+
-                  accel_arm_x+accel_arm_y+accel_arm_z+
-                  accel_belt_x+accel_belt_y+accel_belt_z+
-                  accel_dumbbell_x+accel_dumbbell_y+accel_dumbbell_z
-  , data=training, method="ranger")
+training$classe<-training$classe %>% as.numeric()
 
-pred_rf <- predict(mod_rf,testing)
+testing$classe<-testing$classe %>% as.factor() %>% as.numeric()
 
-confusionMatrix(pred_rf,testing$classe)
+xgb_model <- xgboost(data = as.matrix(training.treat), # training data as matrix
+                          label = training$classe-1,  # column of outcomes
+                          nrounds = 1000,       # number of trees to build
+                          objective          = "multi:softmax",   # default = "reg:linear"
+                          num_class          = 5,
+                          eta = .3,
+                          depth = 6,
+                          verbose = 0  # silent
+)
+
+xgb_result<- predict(xgb_model,as.matrix(testing.treat))+1
+
+confusionMatrix(xgb_result,testing$classe)
